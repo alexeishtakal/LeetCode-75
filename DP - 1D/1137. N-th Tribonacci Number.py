34a57ed86963f16efd5fef123fcd70574df42fd1
alexeishtakal <98085458+alexeishtakal@users.noreply.github.com>
2024-04-15 11:44:46 +0200

    dp problem

--- DP - 1D/1137. N-th Tribonacci Number.py
+++ DP - 1D/1137. N-th Tribonacci Number.py
@@ -1,13 +1,13 @@
 class Solution:
-    def rec(self, n):
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
-        return self.rec(n - 1) + self.rec(n - 2) + self.rec(n - 3)
+    # def rec(self, n):
+    #     if n == 0:
+    #         return 0
+    #     if n == 1 or n == 2:
+    #         return 1
+    #     return self.rec(n - 1) + self.rec(n - 2) + self.rec(n - 3)
 
     def tribonacci(self, n: int) -> int:
-        return self.rec(n)
+        #return self.rec(n)
         tri = []
         tri.append(0)
         tri.append(1)
